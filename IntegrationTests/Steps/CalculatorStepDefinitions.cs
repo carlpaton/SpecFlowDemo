ab0ed6c8994c006f028d4c30bdaacad763452aba
Carl Paton <carl.paton@xero.com>
2020-10-01 21:42:32 +1300

    initial

---
+++ IntegrationTests/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,43 @@
+﻿using TechTalk.SpecFlow;
+
+namespace IntegrationTests.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            _scenarioContext.Pending();
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            _scenarioContext.Pending();
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            //TODO: implement act (action) logic
+            _scenarioContext.Pending();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            //TODO: implement assert (verification) logic
+            _scenarioContext.Pending();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# SpecFlowDemo
+
+Demo code I used to learn how to use Specflow and Gherkin Syntax (Given-When-Then). 
+
+https://carlpaton.github.io/2020/10/specflow/
